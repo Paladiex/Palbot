9dba8311cf3efe03152fd38fbe7baf95ea25681b
Paladiex <30876951+Paladiex@users.noreply.github.com>
2017-08-23 19:12:08 -0500

    Initial online release

---
+++ test.lua
@@ -0,0 +1,27 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Zack
+-- Date: 8/23/2017
+-- Time: 7:07 PM
+-- To change this template use File | Settings | File Templates.
+--
+local loc1 = Location(732, 364)
+local r1, g1, b1 = getColor(loc1)
+local loc2 = Location(715, 364)
+local r2, g2, b2 = getColor(loc2)
+local loc3 = Location(698, 364)
+local r3, g3, b3 = getColor(loc3)
+local loc4 = Location(681, 364)
+local r4, g4, b4 = getColor(loc4)
+local loc5 = Location(664, 364)
+local r5, g5, b5 = getColor(loc5)
+local loc6 = Location(647, 364)
+local r6, g6, b6 = getColor(loc6)
+
+print("r: " .. r1 .. "g: " .. g1 .. "b: " .. b1)
+print("r: " .. r2 .. "g: " .. g2 .. "b: " .. b2)
+print("r: " .. r3 .. "g: " .. g3 .. "b: " .. b3)
+print("r: " .. r4 .. "g: " .. g4 .. "b: " .. b4)
+print("r: " .. r5 .. "g: " .. g5 .. "b: " .. b5)
+print("r: " .. r6 .. "g: " .. g6 .. "b: " .. b6)
+scriptexit()
